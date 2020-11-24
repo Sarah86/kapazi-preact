f7a33f2d4680f668a7757b368538d9a058e00230
Sarah86 <sarah@DESKTOP-VI58DL9.localdomain>
2020-11-24 15:13:50 +0100

    mudança email

--- src/components/infos.js
+++ src/components/infos.js
@@ -13,7 +13,7 @@ export const contatos = {
     numerosp: "+5511941779925",
     whatsappsp: "https://wa.me/5511941779925?text=Ol%C3%A1!%20Estou%20no%20site%20da%20Kapazi%20N%C3%A1utica%20Por%20A%C3%AD%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es.",
     instagram: "https://www.instagram.com/kapazinauticaporai",
-    email: "kapazinauticaporai@gmail.com",
+    email: "kapaziporai@gmail.com",
     endereco: "AV ENGENHEIRO WINSTON MARUCA, 10 - LJ 14 - ANGRA DOS REIS - RJ",
     maps: "https://goo.gl/maps/TBp7nLQMBkexgtYD9"
 }
